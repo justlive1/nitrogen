ff6090349c694f301948bfbe6b9b41900aab50df
justlive1 <qq11419041@163.com>
2018-11-30 18:00:21 +0800

    修改背景图加载方式

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,28 @@
     name: 'app',
     components: {
       Desktop, Dock, Tools
+    },
+    mounted() {
+      document.getElementById('app').style.backgroundImage = 'url(images/main.jpg)';
     }
   }
 </script>
 
 <style>
+  html {
+    height: 100%;
+  }
 
+  body {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+    background-size: 100% 100%;
+  }
 </style>
