d0872300a038adbdd1a1642670fd4f7a19558f0e
justlive1 <qq11419041@163.com>
2018-12-30 22:22:12 +0800

    safari

--- src/components/Safari.vue
+++ src/components/Safari.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="safari">
-
+    <div class="safari-bar">
+      <div class="safari-bar-operation">
+        <i class="fa fa-circle safari-bar-operation-close"></i>
+        <i class="fa fa-circle safari-bar-operation-minus"></i>
+        <i class="fa fa-circle safari-bar-operation-full"></i>
+      </div>
+      <div class="safari-href"></div>
+    </div>
+    <div class="safari-content">
+    </div>
   </div>
 </template>
 
